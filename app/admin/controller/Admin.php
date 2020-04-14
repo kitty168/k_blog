01acc064ebd8a64c42e21c092402d9ef4b633be6
kitty.cheng <450038893@qq.com>
2020-04-15 00:06:23 +0800

    .....

--- app/admin/controller/Admin.php
+++ app/admin/controller/Admin.php
@@ -89,6 +89,12 @@ class Admin extends Base
         return $this->fetch();
     }
 
+    /**
+     * @return string|\think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
     public function edit()
     {
         if($this->request->isPost()){
